827592ac5bba71a1d7b5e6371b568f26578d2732
unknown <kevinchoinj@yahoo.com>
2019-05-16 17:22:26 -0400

    updated react-scripts version; replaced future deprecated NODE_PATH; replaced future deprecated componentWillReceiveProps

--- client/src/pages/SiteRoutesWrapper.js
+++ client/src/pages/SiteRoutesWrapper.js
@@ -4,59 +4,47 @@ import {connect} from 'react-redux';
 import * as transitionActions from 'actions/transition';
 import SiteRoutes from 'pages/SiteRoutes';
 
-class SiteRoutesWrapper extends React.Component {
+class SiteRouteswrapper extends React.Component {
 
   componentDidMount(){
     let currentName = this.props.location.pathname;
     this.props.transitionActions.loadContent(currentName, true);
   }
-
   startTransition = (status, currentName, nextName) => {
     this.props.transitionActions.startTransition(status);
     this.props.transitionActions.loadContent(currentName, false);
     this.props.transitionActions.loadContent(nextName, true);
   }
-
-  componentWillReceiveProps(nextProps) {
-    if ((nextProps.location !== this.props.location) && (this.props.location.pathname==='/')) {
-      let currentName = this.props.location.pathname;
-      let nextName = nextProps.location.pathname;
-      this.startTransition('end', currentName, nextName);
-      setTimeout(function(){
+  componentDidUpdate(prevProps) {
+    const current = this.props.location;
+    const previous = prevProps.location;
+    if (previous.pathname==='/') {
+      this.startTransition('end', previous.pathname, current.pathname);
+      setTimeout(() =>{
         this.props.transitionActions.startTransition('reset');
-      }.bind(this), 400);
+      }, 400);
     }
-    else if (nextProps.location !== this.props.location) {
+    else if (current !== previous) {
       this.props.transitionActions.startTransition('start');
-      let currentName = this.props.location.pathname;
-      let nextName = nextProps.location.pathname;
-      this.props.transitionActions.previousPageName(currentName);
-
-      setTimeout(function(){
-        this.startTransition('end', currentName, nextName);
-      }.bind(this), 700);
-      setTimeout(function(){
+      setTimeout(() => {
+        this.startTransition('end', previous.pathname, current.pathname);
+      }, 700);
+      setTimeout(() => {
         this.props.transitionActions.startTransition('reset');
-      }.bind(this), 800);
+      }, 800);
     }
   }
-
   render() {
+
     return (
-      <div>
-        <div className="text_hidden">
-          <strong>Ashley Wildcat</strong>
-        </div>
-        <SiteRoutes/>
-      </div>
+      <SiteRoutes/>
     );
   }
 }
-
 export default connect(
   () => ({
   }),
   dispatch => ({
     transitionActions: bindActionCreators(transitionActions, dispatch),
   }),
-)(SiteRoutesWrapper);
+)(SiteRouteswrapper);
